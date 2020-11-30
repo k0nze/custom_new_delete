1de0df4fed813336917b374c528da71183259000
Konstantin Luebeck <konstantin.luebeck@uni-tuebingen.de>
2020-11-30 11:00:56 +0100

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <cstdlib>
+
+class Test {
+    private:
+        int m_x;
+        int m_y;
+
+    public:
+        Test() {
+            m_x = 0;
+            m_y = 0;
+        }
+
+        void set_x(int x) {
+            m_x = x;
+        }
+
+        void set_y(int y) {
+            m_y = y;
+        }
+
+        int add_xy() {
+            return m_x + m_y;
+        }
+
+        ~Test() {
+        }
+};
+
+int main() {
+    Test* t = new Test;
+
+    t->set_x(42);
+    t->set_y(77);
+
+    printf("x+y=%d\n", t->add_xy());
+
+    return 0;
+}
